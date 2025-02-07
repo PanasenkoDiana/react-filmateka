b2fd155e16b446a52d3d5adb326b36f6500f2438
PanasenkoDiana <shad0wdrag0n19902@gmail.com>
2025-02-07 21:54:39 +0200

    main, context

--- src/App.tsx
+++ src/App.tsx
@@ -3,19 +3,20 @@ import { Layout } from './shared/Layout/Layout'
 import { Main } from './pages/Main/Main'
 import { MoviesPage } from './pages/MoviesPage/MoviesPage'
 import { MoviePage } from "./pages/MoviePage/MoviePage"
+import { RecentlyViewedProvider } from "./context/RecentlyViewedContext"
 
 export function App() {
     return (
-        <div>
+        <RecentlyViewedProvider>
             <BrowserRouter>
                 <Routes>
-                    <Route element={<Layout/>}>
-                        <Route path='/' element={<Main/>} />
-                        <Route path='/movies' element={<MoviesPage/>} />
-                        <Route path='/movie/:id' element={<MoviePage/>} />
+                    <Route element={<Layout />}>
+                        <Route path='/' element={<Main />} />
+                        <Route path='/movies' element={<MoviesPage />} />
+                        <Route path='/movie/:id' element={<MoviePage />} />
                     </Route>
                 </Routes>
             </BrowserRouter>
-        </div>
-    )
+        </RecentlyViewedProvider>
+    );
 }
