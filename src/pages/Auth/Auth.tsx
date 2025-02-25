c2c689ab66655e2e243aa87812a43c1f515b0840
Nazar Zozulya <likeemangames@gmail.com>
2025-02-25 16:28:47 +0200

    add auth

--- src/pages/Auth/Auth.tsx
+++ src/pages/Auth/Auth.tsx
@@ -6,16 +6,68 @@ export function Auth() {
     const [email, setEmail] = useState('');
     const [password, setPassword] = useState('');
 
-    const handleSubmit = (e: React.FormEvent) => {
+    async function submitAuth(e: React.FormEvent){
         e.preventDefault();
         console.log('Login attempt with:', { email, password });
-    };
+        const response = await fetch('http://localhost:8000/api/user/auth/', {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json',
+            },
+            body: JSON.stringify({
+                email,
+                password,
+            }),
+        })
+
+        const result = await response.json()
+
+        if (result.status === 'error'){
+            alert('Login attempt failed')
+        }
+        else {
+            localStorage.setItem('token', result.data);
+            // window.location.href = '/';
+        }
+    }
+
+    // const handleSubmit = (e: React.FormEvent) => {
+    //     e.preventDefault();
+    //     console.log('Login attempt with:', { email, password });
+    //     const auth = fetch('http://localhost:8000/api/user/auth/', {
+    //         method: 'POST',
+    //         headers: {
+    //             'Content-Type': 'application/json',
+    //         },
+    //         body: JSON.stringify({
+    //             email,
+    //             password,
+    //         }),
+    //     })
+
+    //     const result = 
+
+    //     if (auth.status === 'error')
+    // };
+
+    // async function authFunc(){
+    //     const auth = fetch('http://localhost:8000/api/user/auth/', {
+    //         method: 'POST',
+    //         headers: {
+    //             'Content-Type': 'application/json',
+    //         },
+    //         body: JSON.stringify({
+    //             email,
+    //             password,
+    //         }),
+    //     })
+    // }
 
     return (
         <div className="auth-container">
             <div className="auth-card">
                 <h2>Авторизація</h2>
-                <form onSubmit={handleSubmit}>
+                <form onSubmit={submitAuth}>
                     <input
                         type="text"
                         placeholder="Пошта або телефон"
